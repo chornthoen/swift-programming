2237dc20fa4a2c39e9b444aba2f6c6ec0a24032e
chornthoen <thoenchorn@gmail.com>
2024-03-11 15:25:57 +0700

    Update code add inhertince

--- struct_and_class.swift
+++ struct_and_class.swift
@@ -1,35 +1,46 @@
 
-
-//classes 
-class Student{
-    //properties
+//base class person
+class Person{
     var name: String;
     var age: Int;
-    var school: String;
-
-    //init is a constructor
-    init(name: String, age: Int, school: String){
+    init(name: String, age: Int){
         self.name = name
         self.age = age
-        self.school = school
     }
     //constructor with default value
     init(){
-        self.name = "Thoen Thoen"
+        self.name = "Thoen"
         self.age = 20
-        self.school = "Royal University of Phnom Penh"
+    }
+    //methods for displaying info
+    func printInfoBase(){
+        print("Name: \(name), Age: \(age)")
+    }
+}
+
+
+// derived class student
+class Student : Person{
+    //properties
+    var school: String;
+    var year: Int;
+
+    //init is a constructor
+    init(name: String, age: Int, school: String, year: Int){
+        self.school = school
+        self.year = year
+        super.init(name: name, age: age) //call the base class constructor
     }
     
 
     //methods for displaying info
     func printInfo(){
-        print("Name: \(name), Age: \(age), School: \(school)")
+        print("Name: \(name), Age: \(age), School: \(school) year: \(year)")
+
     }
 
 }
-var student = Student(name:"Chorn Thoen",age:20,school: "Royal University of Phnom Penh")
-var student1 = Student()
+var student = Student(name:"Chorn Thoen",age:20,school: "Royal University of Phnom Penh",year: 4)
 
 student.printInfo() //Name: Chorn Thoen, Age: 20, School: Royal University of Phnom Penh
-student1.printInfo() //Name: Thoen Thoen, Age: 20, School: Royal University of Phnom Penh
 
